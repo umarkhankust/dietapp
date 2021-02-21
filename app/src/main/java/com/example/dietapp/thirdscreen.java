76d98755c77512f97052587f9f0903087dde1713
me-haseeb <bolahaseeb@gmail.com>
2021-02-21 11:47:53 +0500

    added bmi funcionality by haseeb

--- app/src/main/java/com/example/dietapp/thirdscreen.java
+++ app/src/main/java/com/example/dietapp/thirdscreen.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class thirdscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_thirdscreen);
+        TextView bmi =findViewById(R.id.bmi);
+        Bundle bundle = getIntent().getExtras();
+        String message = bundle.getString("bmi");
+//        Log.d("bmi",message+"");
+        bmi.setText(message.substring(0, 5));
+
     }
 
     public void move(View view)
